9c927cba677fb10678cde6c44034c009e259054c
yongjiezhu <1063434281@qq.com>
2023-05-08 16:16:46 +0800

    [feat] 首次提交

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-25 09:07:37
+ * @LastEditTime: 2021-03-25 09:12:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \tiny-heart-master\birthday-mobile\js\index.js
+ */
+$("#login-button").click(function (event) {
+  var userName = document.getElementById("userName").value;
+  var pwd = document.getElementById("pwd").value;
+  //修改密码请改此处
+  console.log(userName)
+  console.log(pwd)
+  if (userName == "宝贝" && pwd == "520") {
+    event.preventDefault();
+    $("form").fadeOut(500);
+    $(".wrapper").addClass("form-success");
+    setTimeout(function () {
+      location.href = "Memories.html";
+    }, 2000);
+  } else {
+    alert("Wrong Password");
+  }
+});
